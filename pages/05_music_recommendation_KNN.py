a7dfd444d4b3ef0b755a1cc0f0093e5421a8ea76
deepen-stha <deepenshrestha1@gmail.com>
2024-04-24 04:12:10 +0530

    making UI better for musix recommendation using KNN

--- pages/05_music_recommendation_KNN.py
+++ pages/05_music_recommendation_KNN.py
@@ -5,7 +5,6 @@ from sklearn.neighbors import NearestNeighbors
 import seaborn as sns
 from datasets import names
 
-
 # Custom color palette for plots
 sns.set_palette("pastel")
 
@@ -20,19 +19,16 @@ st.markdown("""
         font-weight: bold;
     }
     .stApp {
-        background-image: url("https://cdn.pixabay.com/photo/2017/08/06/22/01/books-2596809_960_720.jpg");
+        background-image: url("https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg");
         background-attachment: fixed;
         background-size: cover;
     }
     </style>
     """, unsafe_allow_html=True)
 
-# Load data
-# @st.cache(allow_output_mutation=True)
 @st.cache_data
 def load_data():
     data = pd.read_csv(names.SPOTIFY_DATA)
-    # Normalize the features for the recommendation system
     features = ['danceability', 'energy', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
     data[features] = (data[features] - data[features].min()) / (data[features].max() - data[features].min())
     return data
@@ -48,6 +44,9 @@ user_preferences = {}
 for feature in ['danceability', 'energy', 'valence', 'tempo']:
     user_preferences[feature] = st.sidebar.slider(f'{feature.capitalize()}', 0.0, 1.0, 0.5, 0.01)
 
+# Input for number of recommendations min value will be 2 max value will be 6 and default value is 5
+number_of_recommendations = st.sidebar.number_input('Number of Tracks to Recommend', min_value=2, max_value=6, value=5)
+
 # Recommendation function using nearest neighbors
 def recommend_tracks(preferences, data, n_recommendations=5):
     user_df = pd.DataFrame([preferences])
@@ -58,8 +57,7 @@ def recommend_tracks(preferences, data, n_recommendations=5):
 
 # SQL Query Input
 st.sidebar.subheader('SQL Query for Data Filtering')
-user_query = st.sidebar.text_area("Write your SQL query here (use 'data' as table name):", 
-                                  'SELECT * FROM data LIMIT 10')
+user_query = st.sidebar.text_area("Write your SQL query here (use 'data' as table name):", 'SELECT * FROM data LIMIT 10')
 
 # Execute SQL Query
 query_results = None
@@ -76,7 +74,7 @@ if query_results is not None:
 # Display recommendations based on user preferences
 st.subheader('🎧 Track Recommendations Based on Your Preferences')
 if st.button('Recommend Tracks'):
-    recommended_tracks = recommend_tracks(user_preferences, data)
+    recommended_tracks = recommend_tracks(user_preferences, data, n_recommendations=number_of_recommendations)
     st.write(recommended_tracks[['artists', 'track_name', 'album_name'] + list(user_preferences.keys())])
 
     # Display audio features of recommended tracks using seaborn pairplot
