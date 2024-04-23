1cd38f62841619d010f42a61f12001e00d5e6ce2
Indraneel Rajeevan <e.indraneel@gmail.com>
2024-04-24 02:30:57 +0530

    replaced sample code with homepage initialization

--- app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title('My Multi-Page Streamlit App')
-st.write('This is the main page. Select a page from the dropdown in the sidebar.')
--- main.py
+++ main.py
@@ -1,51 +1,11 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
 
-from datasets import names
+headline = 'Audio Aura'
 
-# Function to load data
-@st.cache_data
-def load_data():
-    data = pd.read_csv(names.SPOTIFY_DATA)
-    return data
-
-# Load data
-data = load_data()
-
-# Streamlit webpage title
-st.title('Music Tracks Data Visualization')
-
-# Sidebar for user input features
-st.sidebar.header('Filters')
-
-# Filter by Genre
-selected_genre = st.sidebar.selectbox('Genre', options=['All'] + list(data['track_genre'].unique()))
-if selected_genre != 'All':
-    data = data[data['track_genre'] == selected_genre]
-
-# Filter by Artist
-selected_artist = st.sidebar.selectbox('Artist', options=['All'] + list(data['artists'].unique()))
-if selected_artist != 'All':
-    data = data[data['artists'] == selected_artist]
-
-# Filter by Popularity
-popularity_range = st.sidebar.slider('Popularity Range', int(data['popularity'].min()), int(data['popularity'].max()), (50, 100))
-data = data[(data['popularity'] >= popularity_range[0]) & (data['popularity'] <= popularity_range[1])]
-
-# Filter by Explicit Content
-explicit_content = st.sidebar.radio('Explicit Content', options=['All', 'Yes', 'No'])
-if explicit_content == 'Yes':
-    data = data[data['explicit'] == 1]
-elif explicit_content == 'No':
-    data = data[data['explicit'] == 0]
-
-# Main page
-st.header('Track Information')
-st.write('Displaying tracks filtered by selected criteria.')
-
-# Plotting the data
-fig = px.bar(data, x='track_name', y='popularity', hover_data=['artists', 'album_name'], color='popularity')
-st.plotly_chart(fig)
-
-# Run this with `streamlit run your_script_name.py`
+st.set_page_config(
+    page_title=headline,
+    page_icon='🎵',
+    layout='centered',
+    initial_sidebar_state='expanded',
+)
+st.title(headline)
