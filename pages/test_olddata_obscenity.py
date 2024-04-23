15e04796c0645ef00329a11ee39c75ff24d611ca
Indraneel Rajeevan <e.indraneel@gmail.com>
2024-04-24 02:40:31 +0530

    formatted code from commit e9656e1

--- pages/test_olddata_obscenity.py
+++ pages/test_olddata_obscenity.py
@@ -5,34 +5,6 @@ from time import sleep
 
 from datasets import names
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#adjust window size
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Load data
 data = pd.read_csv(names.MUSIC_DATA)
 
@@ -42,12 +14,20 @@ st.title('Visualization of Average Obscenity Over the Years')
 
 # Calculate average obscenity and instrumentalness for each year
 average_obscenity = data.groupby('release_date')['obscene'].mean().reset_index()
-average_instrumentalness = data.groupby('release_date')['instrumentalness'].mean().reset_index()
+average_instrumentalness = (
+    data.groupby('release_date')['instrumentalness'].mean().reset_index()
+)
 
 # Determine y-axis range
-y_min = min(average_obscenity['obscene'].min(), average_instrumentalness['instrumentalness'].min())
-y_max = max(average_obscenity['obscene'].max(), average_instrumentalness['instrumentalness'].max())
-y_range = [max(0, y_min - 0.1), min(1, y_max )]
+y_min = min(
+    average_obscenity['obscene'].min(),
+    average_instrumentalness['instrumentalness'].min(),
+)
+y_max = max(
+    average_obscenity['obscene'].max(),
+    average_instrumentalness['instrumentalness'].max(),
+)
+y_range = [max(0, y_min - 0.1), min(1, y_max)]
 
 # Create plot
 fig = go.Figure()
@@ -61,7 +41,7 @@ fig.add_trace(
         # line=dict(color='gold', width=1.5, shape='spline'),
         line=dict(color='royalblue', width=1.5, shape='spline'),
         name='Average Obscenity',
-        opacity=0.7
+        opacity=0.7,
     )
 )
 
@@ -78,7 +58,9 @@ fig.add_trace(
 
 # Update layout
 fig.update_layout(
-    xaxis=dict(title='Release Date', dtick=10),  # Adjust tick frequency on x-axis for better readability
+    xaxis=dict(
+        title='Release Date', dtick=10
+    ),  # Adjust tick frequency on x-axis for better readability
     yaxis=dict(title='Value', range=y_range),  # Set dynamic range for y-axis
     # title='Visualization of Average Obscenity and Instrumentalness Over the Years',
     title='Visualization of Average Obscenity Over the Years',
@@ -94,8 +76,8 @@ plot = st.plotly_chart(fig)
 for i in range(len(average_obscenity)):
     with plot:
         sleep(0.1)
-        fig.data[0]['x'] = average_obscenity['release_date'][:i+1]
-        fig.data[0]['y'] = average_obscenity['obscene'][:i+1]
+        fig.data[0]['x'] = average_obscenity['release_date'][: i + 1]
+        fig.data[0]['y'] = average_obscenity['obscene'][: i + 1]
         # fig.data[1]['x'] = average_instrumentalness['release_date'][:i+1]
         # fig.data[1]['y'] = average_instrumentalness['instrumentalness'][:i+1]
         plot.plotly_chart(fig, use_container_width=True)
