0dcb971cd542d03420182330aa78730880172920
deepen-stha <deepenshrestha1@gmail.com>
2024-04-26 22:04:51 +0530

    added acknowledgement page at last

---
+++ pages/10_Acknowledgments.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+headline = 'Acknowledgements'
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url('https://images5.alphacoders.com/438/438504.jpg');
+        background-attachment: fixed;
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.title(headline)
+st.markdown("""
+""", unsafe_allow_html=False)
+st.markdown("""
+**The success of this project is attributed to the dedication, expertise, and collaborative efforts of all the team members of Group 1**
+""", unsafe_allow_html=False)
+
+st.markdown("""
+""", unsafe_allow_html=False)
+st.markdown("""**Akash Varude**""",unsafe_allow_html=False)
+st.markdown("""**Darshan Jain**""",unsafe_allow_html=False)
+st.markdown("""**Deepen Shrestha**""",unsafe_allow_html=False)
+st.markdown("""**Indraneel Rajeevan**""",unsafe_allow_html=False)
+st.markdown("""**Nitish Kumar**""",unsafe_allow_html=False)
+st.markdown("""**Pankaj Nandeshwar**""",unsafe_allow_html=False)
+st.markdown("""**Pranjal Nandeshwar**""",unsafe_allow_html=False)
+st.markdown("""**Shaurya Agarwal**""",unsafe_allow_html=False)
